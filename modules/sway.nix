1f32cea7b3d8da6aea6c53f6a75d7e564669b1fb
Anton Iakimov <yakimant@gmail.com>
2025-02-27 13:56:07 +0100

    sway: add login manager

--- modules/sway.nix
+++ modules/sway.nix
@@ -12,4 +12,13 @@
 
   programs.sway.enable = true;
   services.pipewire.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+      };
+    };
+  };
 }
