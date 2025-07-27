cfe2eef204c8d0a2c3f7ac76e02f3e03c4c3f2b9
Anton Iakimov <yakimant@gmail.com>
2025-07-27 13:33:14 +0200

    linux: encrypt pass-hash

--- modules/linux/users.nix
+++ modules/linux/users.nix
@@ -1,8 +1,10 @@
 {
-  ...
-}:
+  age.secrets = {
+    "users/yakimant/pass-hash" = {
+      file = ../../secrets/users/yakimant/pass-hash.age;
+    };
+  };
 
-{
   users.mutableUsers = false;
 
   users.users.yakimant = {
@@ -10,10 +12,10 @@
     description = "yakimant";
     extraGroups = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys = [
-      "no-touch-required sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJXqUxcqsY1U4xG1WlRS0WCaFHeF1MXp3wWhCPuW+b2rAAAADHNzaDpuby10b3VjaA== yakimant@gmail.com"
-      "no-touch-required sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDSUvodIwcGaDJU79/Ds38fLVwBb3EqpTPplYB0xH0qNAAAADHNzaDpuby10b3VjaA== yakimant@gmail.com"
+      "no-touch-required sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJXqUxcqsY1U4xG1WlRS0WCaFHeF1MXp3wWhCPuW+b2rAAAADHNzaDpuby10b3VjaA=="
+      "no-touch-required sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDSUvodIwcGaDJU79/Ds38fLVwBb3EqpTPplYB0xH0qNAAAADHNzaDpuby10b3VjaA=="
     ];
-    hashedPassword = "$y$j9T$3s/rC.yqNyNobdespcGNJ/$PYrHL9yWCdEy4WsjHHXGt9edvLD/YqpVXjZKKKxbKk/";
+    hashedPasswordFile = config.age.secrets."users/yakimant/pass-hash".path;
   };
 
   security.sudo.wheelNeedsPassword = false;
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,14 @@
 let
-  yakimant1 = "age1yubikey1qv8svrrl7grjjeh5qllgpz6wn3c85xsxu87y5hk0untqtsnr36tjxrwzt0d";
-  yakimant2 = "age1yubikey1q28qjdfnktl7rdd4wk84tgt3jx30nm8m7ac4x5qcw57ujtqtzfqexkm67tf";
-  yakimant = [ yakimant1 yakimant2 ];
+  yakimant = [
+    "age1yubikey1qv8svrrl7grjjeh5qllgpz6wn3c85xsxu87y5hk0untqtsnr36tjxrwzt0d"
+    "age1yubikey1q28qjdfnktl7rdd4wk84tgt3jx30nm8m7ac4x5qcw57ujtqtzfqexkm67tf"
+  ];
+  users = yakimant;
 
   qnap = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPsHtmRlKCqa8n8p/KVJJ5GOJQccpjnlbkecqhqi5086";
+  servers = [ qnap ];
 in
 {
   "service/tailscale/qnap.age" = { publicKeys = yakimant ++ [ qnap ]; };
+  "users/yakimant/pass-hash.age" = { publicKeys = yakimant ++ servers; };
 }
