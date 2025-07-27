5359ff160570787e5b7216b3741bf4e9d2cd7e97
Anton Iakimov <yakimant@gmail.com>
2025-07-27 13:19:33 +0200

    linux: cleanup ssh keys

--- modules/linux/users.nix
+++ modules/linux/users.nix
@@ -12,8 +12,6 @@
     openssh.authorizedKeys.keys = [
       "no-touch-required sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJXqUxcqsY1U4xG1WlRS0WCaFHeF1MXp3wWhCPuW+b2rAAAADHNzaDpuby10b3VjaA== yakimant@gmail.com"
       "no-touch-required sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDSUvodIwcGaDJU79/Ds38fLVwBb3EqpTPplYB0xH0qNAAAADHNzaDpuby10b3VjaA== yakimant@gmail.com"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDoGVex1+pNWNMwC5dygX0WnYcythWB76c5s+M68JKsETiTNeJRbRqB5vMiaeeb0uTXdwNeic5inJkF4VDkW53o2v2+c5HbizgXB3VlxdZJDaK3eK6QfQ4QfUsvphdh5BgFTCvCj0u2Jja+lupsNg68jQirTDTyKo8nrWbYnWGGgjMDLUZ3iO3XmjGEH82izfyg4IWGum4+58hPwTfqn+sO1M6mEUgwgtgUmXPXk5W2xlrzVVNtfpUU1rCLjpdIP8dStFUczm9u9M1hYAGhH9XuqYY4L1bKyHhb20hKlujItudLxN6H7iNy3tOLFvpNRib9euhoqRKMuROtucANKRoA6HM7JvpBykbWuEHDmxWsy2CCGxaNJbsexkEqzCiULBLIxBL2KSFX0rp4BZ9VqrfwPTcl60Mh0DbRDzGAAiEnHRk9Mjz/L1enGk7Gtqso2uyi6pi7b9r/1znkKJUfSRALRMdcnm6wghSvNA/WHKCDxwKY31DUw7kKZXeyjZbbQ/8U1+84jleT8xMdAECz/lhVD2jQ5rrD8hchziXjvKcUT4lzXW8gZ5LwXLC4AJC7Wvq/Hue1HKJDTMCx82uWRPCLhO+ZbUnorsdvkFhoVN7A5QeQIWp8C6xZPGEPej/4v9l/iB2bKv6v7TTlnJrCM6CSRP2XhblsvXaGo+tjEHaKxw=="
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcivTJT6l2oSSKqmON+CAH8j1NwLkW7RtZkDnXfBnMv"
     ];
     hashedPassword = "$y$j9T$3s/rC.yqNyNobdespcGNJ/$PYrHL9yWCdEy4WsjHHXGt9edvLD/YqpVXjZKKKxbKk/";
   };
