641e7eaae687e40e051caaa5a28abec42cb6702d
Anton Iakimov <yakimant@gmail.com>
2025-08-11 11:19:07 +0200

    darwin: hourly apfs snapshots

--- modules/darwin/apfs.nix
+++ modules/darwin/apfs.nix
@@ -1,13 +1,14 @@
 {
+  # macOS is purging older that 24h (probably Time Machine)
   launchd.daemons.apfs-snapshot = {
-    # TODO: keep 14 (2 weeks)
     serviceConfig = {
       ProgramArguments = [
         "/usr/bin/tmutil"
         "snapshot"
       ];
-      RunAtLoad = true;
-      StartInterval = 86400;
+      StartCalendarInterval = [{
+        Minute = 0;
+      }];
     };
   };
 }
