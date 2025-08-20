bbc149891374e94867fb0d371daff0e6f2f6bc55
Anton Iakimov <yakimant@gmail.com>
2025-08-20 16:51:39 +0200

    linux-desktop/keyd: add a second keyboard

--- modules/linux-desktop/keyd.nix
+++ modules/linux-desktop/keyd.nix
@@ -1,25 +1,47 @@
 {
   services.keyd = {
     enable = true;
-    keyboards.default.settings = {
-      main = {
-        capslock = "overload(control, esc)";
-        leftalt = "layer(control)";
-        control = "layer(nav)";
-        left = "noop";
-        right = "noop";
-        up = "noop";
-        down = "noop";
+    keyboards = {
+      thinkpad = {
+        ids = ["0001:0001:70533846"];
+        settings = {
+          main = {
+            capslock = "overload(control, esc)";
+            leftalt = "layer(control)";
+            control = "layer(nav)";
+            left = "noop";
+            right = "noop";
+            up = "noop";
+            down = "noop";
+          };
+          control = {
+            # TODO: disable later
+            #"[" = "noop";
+            #"v" = "noop";
+          };
+          nav = {
+            h = "left";
+            j = "down";
+            k = "up";
+            l = "right";
+          };
+        };
       };
-      control = {
-        # TODO: disable later
-        #"[" = "noop";
-      };
-      nav = {
-        h = "left";
-        j = "down";
-        k = "up";
-        l = "right";
+
+      pok3r = {
+        ids = ["0f39:0618:67105f83"];
+        settings = {
+          main = {
+            #capslock = "overload(control, esc)";
+            meta = "layer(control)";
+            leftalt = "layer(meta)";
+          };
+          control = {
+            # TODO: disable later
+            #"[" = "noop";
+            #"v" = "noop";
+          };
+        };
       };
     };
   };
