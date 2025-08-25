1f9c5444a144620964a87cff1bfbabccfa2311d7
Anton Iakimov <yakimant@gmail.com>
2025-08-25 18:37:57 +0200

    linux-desktop/packages: add more tools

--- modules/linux-desktop/packages.nix
+++ modules/linux-desktop/packages.nix
@@ -6,7 +6,11 @@
   environment.systemPackages = with pkgs; [
     _1password-gui
     alacritty
+    bitwarden-desktop
+    discord
     firefox
+    libnotify
+    playerctl
     spotify
     telegram-desktop
   ];
