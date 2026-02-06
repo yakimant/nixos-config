acbee066923461cb6418b30180ebf559ee471465
Anton Iakimov <yakimant@gmail.com>
2026-02-06 10:56:00 +0100

    secrets: rekey for new validator key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,11 +3,10 @@ let
     "age1yubikey1qfedhpwa0jdnflnvkyw0au3r2slh68wqyna6mzjee4206a6vp0xqshlr8r3"
     "age1yubikey1qfp23hg0l5t5fgtac4hrfz9dk9mjrfvjuujhhelxkwh3309hej8ju0cytuh"
   ];
-  users = yakimant;
 
   qnap = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPsHtmRlKCqa8n8p/KVJJ5GOJQccpjnlbkecqhqi5086";
   thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEGpYbcECL6aRWEpGZLKcGwlR3Fuejf9XiXrh/NHMeD";
-  validator = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJEA6r8W9z7mrkBwbxi/fS0+McuCf088gClQ8WySm/zQ";
+  validator = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpXAqAALOropAbctZP545fRnrfWB9fOelFtG8mzxV/G";
   linux = [ qnap thinkpad validator ];
 in
 {
