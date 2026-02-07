3303f2ceb6527ebcbc1de7cba1f4bbaa73194963
Anton Iakimov <yakimant@gmail.com>
2026-02-07 10:01:01 +0100

    metrics: fix interface name

--- modules/linux/metrics.nix
+++ modules/linux/metrics.nix
@@ -5,7 +5,7 @@
 
 let
   cfg = config.services.prometheus.exporters.node;
-  tsCfg = config.services.tailscale.interfaceName;
+  vpnInterfaceName = config.services.tailscale.interfaceName;
 in {
   services.prometheus.exporters.node = {
     enable = true;
@@ -17,9 +17,9 @@ in {
     disabledCollectors = [ "textfile" ];
     openFirewall = true;
     # iptables:
-    #firewallFilter = "-i ${tsCfg.interfaceName} -p tcp -m tcp --dport ${cfg.port}";
+    #firewallFilter = "-i ${vpnInterfaceName} -p tcp -m tcp --dport ${cfg.port}";
     firewallRules = ''
-      iifname "${tsCfg.interfaceName}" tcp dport ${cfg.port} counter accept
+      iifname "${vpnInterfaceName}" tcp dport ${cfg.port} counter accept
     '';
   };
 }
