a2e1a83f3627dde8865302409753005662aa0ac1
Anton Iakimov <yakimant@gmail.com>
2026-02-25 10:44:26 +0100

    nix: add numtide cache

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -36,10 +36,12 @@
         "https://infra-lido.cachix.org"
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://cache.numtide.com"
       ];
       trusted-public-keys = [
         "infra-lido.cachix.org-1:NX5g5Ybi4ZwdZJWPmh7JAHntqvtWesyPfAJwcRvyJGw="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
   };
